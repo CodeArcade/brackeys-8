773ae1e9aab9a70881f08b114163003c8acc3356
Cyklan <dev@cyklan.de>
2022-08-23 11:20:12 +0200

    added river directions to t-tile

--- game/src/models/game/tTile.ts
+++ game/src/models/game/tTile.ts
@@ -1,9 +1,11 @@
 import { Tile as BaseTile } from "@models";
+import { Rotation } from "models/level/tile";
 import { Tile } from "./tile";
 import { TileDimensions } from "./tileDimensions";
 
 export class TTile extends Tile {
   constructor(tile: BaseTile, size: TileDimensions, x: number, y: number) {
     super(tile, size, x, y);
+    this.riverEnds = [Rotation.Left, Rotation.Top, Rotation.Right]
   }
 }
