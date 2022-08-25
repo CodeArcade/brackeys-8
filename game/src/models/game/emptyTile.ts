4729091c6827cf7e8b6decc4b50baea3a4b04623
Cyklan <dev@cyklan.de>
2022-08-25 09:02:21 +0200

    decide decoration rotation based on case by case basis instead of tile rotation

--- game/src/models/game/emptyTile.ts
+++ game/src/models/game/emptyTile.ts
@@ -14,65 +14,72 @@ export class EmptyTile extends Tile {
     "grass2-",
     "stoneFlat",
     "yellowMushroom",
-    "pebble"
-  ]
+    "pebble",
+  ];
 
   private readonly decorationPoints: Vector2[] = [
-    {x: 10, y: 13},
-    {x: 10, y: 30},
-    {x: -5, y: 50},
-    {x: -35, y: 40},
-    {x: 35, y: 40},
-    {x: 30, y: 55 },
-    {x: 0, y: 70}
-  ]
+    { x: 10, y: 13 },
+    { x: 10, y: 30 },
+    { x: -5, y: 50 },
+    { x: -35, y: 40 },
+    { x: 35, y: 40 },
+    { x: 30, y: 55 },
+    { x: 0, y: 70 },
+  ];
 
   constructor(tile: BaseTile, size: TileDimensions, x: number, y: number) {
     super(tile, size, x, y);
 
     this.normalTexture = Texture.from("blockedTile0");
-    this.sprite.texture = this.normalTexture
+    this.sprite.texture = this.normalTexture;
 
     this.buttonMode = true;
 
-    this.baseTile.rotation = Math.floor(Math.random() * 4)
+    const decorationAmount = Math.max(2, Math.floor(Math.random() * 5));
 
-    const decorationAmount = Math.max(2, Math.floor(Math.random() * 5))
+    let usedDecorationPoints: number[] = [];
 
-    let usedDecorationPoints: number[] = []
-
-    let spritesToRender: Sprite[] = []
+    let spritesToRender: Sprite[] = [];
 
     for (let i = 0; i < decorationAmount; i++) {
-      const randomDecoration = this.decorationItems[Math.floor(Math.random() * this.decorationItems.length)]
-      const sprite = new Sprite(Texture.from(
-        this.getDecorationRotation(randomDecoration)
-        ));
-        let point = -1;
+      const decorationRotation = Math.floor(Math.random() * 4);
+      const randomDecoration =
+        this.decorationItems[
+          Math.floor(Math.random() * this.decorationItems.length)
+        ];
+      const sprite = new Sprite(
+        Texture.from(
+          this.getDecorationRotation(
+            randomDecoration,
+            Math.floor(Math.random() * 4)
+          )
+        )
+      );
+      let point = -1;
       do {
-        point = Math.floor(Math.random() * this.decorationPoints.length)
-      } while (point < 0 || usedDecorationPoints.includes(point))
-      usedDecorationPoints.push(point)
-      sprite.x = this.decorationPoints[point].x + 95
-      sprite.y = this.decorationPoints[point].y - 110
-      sprite.anchor.set(0, 1)
+        point = Math.floor(Math.random() * this.decorationPoints.length);
+      } while (point < 0 || usedDecorationPoints.includes(point));
+      usedDecorationPoints.push(point);
+      sprite.x = this.decorationPoints[point].x + 95;
+      sprite.y = this.decorationPoints[point].y - 110;
+      sprite.anchor.set(0, 1);
 
       if (randomDecoration === "stoneFlat" || randomDecoration === "pebble") {
-        sprite.scale.set(0.3)
+        sprite.scale.set(0.3);
       } else if (randomDecoration.startsWith("grass")) {
-        sprite.scale.set(0.7)
+        sprite.scale.set(0.7);
       }
 
-      spritesToRender.push(sprite)
+      spritesToRender.push(sprite);
     }
 
-    spritesToRender = spritesToRender.sort((a, b) => a.y > b.y ? 1 : -1)
+    spritesToRender = spritesToRender.sort((a, b) => (a.y > b.y ? 1 : -1));
     for (let s of spritesToRender) {
       this.addChild(s);
     }
   }
 
-  getDecorationRotation = (decorationAsset: string) => {
-    return `${decorationAsset}${this.baseTile.rotation}`
-  }
+  getDecorationRotation = (decorationAsset: string, rotation: number) => {
+    return `${decorationAsset}${rotation}`;
+  };
 }
